bba912ac5c77b7de81955435483befb5e1487018
Surya Sonar <iamsuryasonar@gmail.com>
2023-09-08 15:47:31 +0530

    almost done

--- frontend/src/app/common/constants.js
+++ frontend/src/app/common/constants.js
@@ -1,6 +1,7 @@
 export const APP_NAME = 'URL Share';
 export const LOCAL_STORAGE_NAME = 'user';
-export const API_URL_PROFILE = "https://urlshare-backend.onrender.com/api/profile/";
-export const API_URL_AUTH = "https://urlshare-backend.onrender.com/api/user/";
-export const API_URL_MY_URL = 'https://urlshare-backend.onrender.com/api/url/';
-export const API_URL_URL = 'https://urlshare-backend.onrender.com/api/public_urls/';
+const API_URL = 'https://urlshare-backend.onrender.com'
+export const API_URL_PROFILE = API_URL + "/api/profile/";
+export const API_URL_AUTH = API_URL + "/api/user/";
+export const API_URL_MY_URL = API_URL + '/api/url/';
+export const API_URL_URL = API_URL + '/api/public_urls/';
