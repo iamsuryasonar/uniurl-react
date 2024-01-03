330debbde463a3007fbd2d81d653a75d794e3b9d
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2024-01-03 18:25:29 +0530

    fix:  extra reducers updated to use builder callback

--- frontend/src/app/store/slices/authSlice.js
+++ frontend/src/app/store/slices/authSlice.js
@@ -68,26 +68,23 @@ const initialState = user
 const authSlice = createSlice({
     name: "auth",
     initialState,
-    extraReducers: {
-        [register.fulfilled]: (state, action) => {
-            state.isLoggedIn = false;
-        },
-        [register.rejected]: (state, action) => {
-            state.isLoggedIn = false;
-        },
-        [login.fulfilled]: (state, action) => {
-            state.isLoggedIn = true;
-            state.user = action.payload.user;
-        },
-        [login.rejected]: (state, action) => {
-            state.isLoggedIn = false;
-            state.user = null;
-        },
-        [logout.fulfilled]: (state, action) => {
-            state.isLoggedIn = false;
-            state.user = null;
-        },
-    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(register.fulfilled, (state, action) => {
+                state.isLoggedIn = false;
+            }).addCase(register.rejected, (state, action) => {
+                state.isLoggedIn = false;
+            }).addCase(login.fulfilled, (state, action) => {
+                state.isLoggedIn = true;
+                state.user = action.payload.user;
+            }).addCase(login.rejected, (state, action) => {
+                state.isLoggedIn = false;
+                state.user = null;
+            }).addCase(logout.fulfilled, (state, action) => {
+                state.isLoggedIn = false;
+                state.user = null;
+            })
+    }
 });
 
 const { reducer } = authSlice;
