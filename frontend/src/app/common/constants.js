4cf08e05dde8b0123170f2237523a6634893faca
Surya Sonar <iamsuryasonar@gmail.com>
2024-02-06 00:32:30 +0530

    api url changed

--- frontend/src/app/common/constants.js
+++ frontend/src/app/common/constants.js
@@ -1,7 +1,7 @@
 export const APP_NAME = 'UniURL';
 export const LOCAL_STORAGE_NAME = 'user';
-// const API_URL = process.env.REACT_APP_BASE_URL;
-const API_URL = 'http://localhost:3001'
+const API_URL = process.env.REACT_APP_BASE_URL;
+// const API_URL = 'http://localhost:3001'
 export const API_URL_PROFILE = API_URL + "/api/profile/";
 export const API_URL_AUTH = API_URL + "/api/user/";
 export const API_URL_MY_URL = API_URL + '/api/url/';
