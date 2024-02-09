98798b00fdce98997cb8f9d1d41025179addde54
Surya Sonar <iamsuryasonar@gmail.com>
2024-02-10 01:31:25 +0530

    fix

--- frontend/src/app/common/constants.js
+++ frontend/src/app/common/constants.js
@@ -1,8 +1,9 @@
 export const APP_NAME = 'UniURL';
 export const LOCAL_STORAGE_NAME = 'user';
-// const API_URL = process.env.REACT_APP_BASE_URL;
-const API_URL = 'http://localhost:3002'
-export const API_URL_PROFILE = API_URL + "/api/profile/";
-export const API_URL_AUTH = API_URL + "/api/user/";
-export const API_URL_MY_URL = API_URL + '/api/url/';
-export const API_URL_URL = API_URL + '/api/public_urls/';
+const PRODUCTION_API_URL = process.env.REACT_APP_BASE_URL;
+const LOCAL_API_URL = 'http://localhost:3002'
+
+export const API_URL_PROFILE = process.env.NODE_ENV = 'production' ? PRODUCTION_API_URL : LOCAL_API_URL + "/api/profile/";
+export const API_URL_AUTH = process.env.NODE_ENV = 'production' ? PRODUCTION_API_URL : API_ULOCAL_API_URLRL + "/api/user/";
+export const API_URL_MY_URL = process.env.NODE_ENV = 'production' ? PRODUCTION_API_URL : LOCAL_API_URL + '/api/url/';
+export const API_URL_URL = process.env.NODE_ENV = 'production' ? PRODUCTION_API_URL : LOCAL_API_URL + '/api/public_urls/';
