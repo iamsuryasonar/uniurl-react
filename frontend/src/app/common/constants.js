5827083e4d32e66dd7cb7f0a48525f4aa9ec6ed3
Surya Sonar <iamsuryasonar@gmail.com>
2024-02-10 01:44:22 +0530

    fix

--- frontend/src/app/common/constants.js
+++ frontend/src/app/common/constants.js
@@ -4,6 +4,6 @@ const PRODUCTION_API_URL = process.env.REACT_APP_BASE_URL;
 const LOCAL_API_URL = 'http://localhost:3002'
 
 export const API_URL_PROFILE = process.env.NODE_ENV = 'production' ? PRODUCTION_API_URL : LOCAL_API_URL + "/api/profile/";
-export const API_URL_AUTH = process.env.NODE_ENV = 'production' ? PRODUCTION_API_URL : API_ULOCAL_API_URLRL + "/api/user/";
+export const API_URL_AUTH = process.env.NODE_ENV = 'production' ? PRODUCTION_API_URL : LOCAL_API_URL + "/api/user/";
 export const API_URL_MY_URL = process.env.NODE_ENV = 'production' ? PRODUCTION_API_URL : LOCAL_API_URL + '/api/url/';
 export const API_URL_URL = process.env.NODE_ENV = 'production' ? PRODUCTION_API_URL : LOCAL_API_URL + '/api/public_urls/';
