12a3e3a34c4c8fc78332f6ef56821530a5034889
Surya Sonar <iamsuryasonar@gmail.com>
2024-02-22 18:34:30 +0530

    ui improved from url page

--- frontend/src/app/pages/urls/Urls_page.jsx
+++ frontend/src/app/pages/urls/Urls_page.jsx
@@ -1,11 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { clearMessage } from '../../store/slices/messageSlice';
+import { clearMessage, setMessage } from '../../store/slices/messageSlice';
 import { get_urls } from '../../store/slices/urlSlice';
 import avatar from '../../assets/avatar.jpg'
-
+import Message from '../../components/Message/Message'
+import { Transition } from 'react-transition-group';
 function UrlCard({ urlData, onClick, theme }) {
     const { title, url } = urlData;
 
@@ -23,6 +24,10 @@ function UrlCard({ urlData, onClick, theme }) {
 function UrlsPage() {
     const { username } = useParams();
     const dispatch = useDispatch();
+    const [isSticky, setIsSticky] = useState(false);
+    const [menu, setMenu] = useState(true);
+    const elementRef = useRef(null);
+    const { message } = useSelector((state) => state.message);
 
     useEffect(() => {
         dispatch(clearMessage());
@@ -34,20 +39,104 @@ function UrlsPage() {
         window.open(url, '_blank');
     }
 
+    useEffect(() => {
+        const handleScroll = () => {
+            const itemOffset = elementRef.current.offsetTop;
+            const scrollTop = window.scrollY;
+
+            if (scrollTop >= itemOffset) {
+                setIsSticky(true)
+            } else if (scrollTop < itemOffset) {
+                setIsSticky(false)
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const copyToClipboard = async (name) => {
+        try {
+            const originname = window.location.origin;
+            await navigator.clipboard.writeText(originname + '/' + name);
+            dispatch(setMessage('copied To clipboard'))
+            setTimeout(() => {
+                dispatch(clearMessage());
+            }, 2000)
+        } catch (err) {
+            dispatch(setMessage('could not copy To clipboard'))
+            setTimeout(() => {
+                dispatch(clearMessage());
+            }, 2000)
+        }
+    };
+
+
     return (
         <div style={urlsinfo?.theme?.pagecontainer} className={`${!urlsinfo?.theme ? `max-w-xl min-h-svh bg-slate-100 m-auto flex flex-col p-4 gap-4` : ''}`}>
             {urlsinfo ?
-                <div className='w-full'>
+                <div className='w-full relative '>
+                    {/* sticky nav */}
+                    <div className={`sticky top-4 left-4 right-4  rounded-full ${isSticky ? 'backdrop-blur-sm bg-white/30 flex flex-row justify-between items-center' : 'grid justify-items-end '}`}>
+                        {isSticky &&
+                            <>
+                                <div className={`w-10 h-10 m-1`}>
+                                    <img src={urlsinfo?.picture?.url ? urlsinfo?.picture?.url : avatar} className='w-full h-full rounded-full object-fit' />
+                                </div>
+                                <p className='text-2xl font-light'>{urlsinfo?.name}</p>
+                            </>
+                        }
+                        <div onClick={() => setMenu(!menu)} className='bg-slate-200 rounded-full w-10 h-10 m-1 grid place-content-center'><FontAwesomeIcon icon="fas fa-ellipsis" /></div>
+                    </div>
+                    {<Transition in={menu} timeout={100}>
+                        {(state) => (
+                            <div className={`fixed sm:top-0 right-0 bottom-0 left-0  rounded-xl flex justify-center items-center transition-transform transform ease-in-out duration-700 ${state === 'entered' ? 'translate-y-0 ' : 'translate-y-full '}`}>
+                                <div className='bg-white max-w-xl w-full sm:w-auto sm:h-5/6 md:m-20  rounded-t-lg sm:rounded-xl  p-6 flex flex-col gap-4'>
+                                    <div className='w-full flex flex-row justify-between items-center'>
+                                        <p className='text-md font-bold'>Welcome to my Uni Url</p>
+                                        <div onClick={() => setMenu(false)} className='hover:bg-slate-200 p-2 grid place-content-center'>
+                                            <FontAwesomeIcon className='text-2xl' icon='fas fa-xmark' />
+                                        </div>
+                                    </div>
+                                    <div onClick={() => copyToClipboard(urlsinfo?.name)} className='relative'>
+                                        <input
+                                            type="text"
+                                            name="name"
+                                            value={urlsinfo?.name}
+                                            maxLength="30"
+                                            placeholder="Alexa"
+                                            className='w-full py-1 px-2 text-xl border border-1 rounded-md border-black'
+                                            readOnly
+                                        />
+                                        <FontAwesomeIcon icon='fas fa-copy' className='absolute top-1/2 right-2 -translate-y-1/2 cursor-pointer text-black' />
+                                    </div>
+                                    <div className='h-[1px] bg-slate-600 w-full'></div>
+                                    <p className='text-xl font-bold'>Create your own Uni Url</p>
+                                    <div className='w-full flex justify-between gap-5 md:gap-2'>
+                                        <button className='w-full bg-black text-white py-1 rounded-full'>Sign up free</button>
+                                        <button className='w-full bg-white text-black py-1 border  rounded-full'>Find out mode</button>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+                    </Transition >
+                    }
+
                     <div style={urlsinfo?.theme?.avatarnamebiocontainer} className={`${!urlsinfo?.theme ? `flex items-center flex-col gap-4 py-10 ` : ''}`}>
-                        <div style={urlsinfo?.theme?.avatarimagecontainer} className={`${!urlsinfo?.theme ? `w-28 h-28 aspect-square rounded-full ` : ''}`}>
+                        <div style={{
+                            ...urlsinfo?.theme?.avatarimagecontainer
+                        }} className={` ${!urlsinfo?.theme ? `w-28 h-28 aspect-square rounded-full ` : ''}`}>
                             <img src={urlsinfo?.picture?.url ? urlsinfo?.picture?.url : avatar} className='w-full h-full object-fit' />
                         </div>
                         <div style={urlsinfo?.theme?.namebiocontainer}>
-                            <p className='text-2xl'>{urlsinfo?.name}</p>
+                            <p ref={elementRef} className='text-2xl'>{urlsinfo?.name}</p>
                             <p className='text-lg'>{urlsinfo?.bio}</p>
                         </div>
                     </div>
-                    <div className='flex flex-col items-center gap-4'>
+                    <div className='flex flex-col items-center gap-4 pb-10'>
                         {
                             urlsinfo && urlsinfo?.links.length > 0 ? urlsinfo?.links.map((item) => {
                                 return (
@@ -59,6 +148,10 @@ function UrlsPage() {
                 </div>
                 :
                 <></>
+            }
+            {
+                message &&
+                <Message message={message} />
             }
         </div >
     );
